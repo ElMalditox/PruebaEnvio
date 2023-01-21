fc20efffa563e7399f166908badfc45789ba416d
ElMalditox <122885425+ElMalditox@users.noreply.github.com>
2023-01-21 05:56:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,34 +4,39 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "./public")));
 const nodemailer = require('nodemailer');
 
-sendMail = async (req, res) => {
-    
-    const transporter = nodemailer.createTransport({
-        port: 465,
+
+
+sendMail = async ()=>{
+
+    const config = {
+        port: 587,
         host: process.env.hostt,
         auth: {
             user: process.env.envio,
             pass: process.env.paswordd
-        },
-        secure: true,
-    });
-    
-    const mailData = {
+        
+    }
+
+}
+    const mensaje = {
         from:process.env.envio ,
-        to: "yosoytfk@gmail.com",
+        to: "jose.c.escobar.v@gmail.com",
         subject: 'hola',
-        text: "llego",
-    };
-    
-    res.status(200).json({ status: "OK" });
-    };
-    app.post("/send-email",(req,res)=>{
-        sendMail()
-        .then((result)=>res.status(200).send("enviado"))
-        .catch((error)=> console.log("error"));
-        console.log("mail enviado")
-    })
+        text: "llego"
 
+    }
+    const transporter = nodemailer.createTransport(config);
 
+    const info = await transporter.sendMail(mensaje);
+
+    console.log(info)
+}
+
+app.post("/send-email",()=>{
+    sendMail()
+    .then((result)=>res.status(200).send("enviado"))
+    .catch((error)=> console.log("error"));
+    console.log("mail enviado")
+})
 app.listen(process.env.PORT || 3000);
 console.log(`server is listening on port `, process.env.PORT || 3000);
